5a69161781beeee99e7af95b828b8968421ee4cb
Rob <robstah72@gmail.com>
2020-11-25 08:09:52 +0000

    Implement Animator class and add it to BaseAlien

---
+++ core/src/com/gwenci/zarrax/Animator.java
@@ -0,0 +1,29 @@
+package com.gwenci.zarrax;
+
+public class Animator {
+	private final float frameTimeInSecs;
+	private float timerInSecs;
+	private final int nFrames;
+	private int currentFrame;
+
+	public Animator(int nFrames, float frameTimeInSecs) {
+		this.nFrames = nFrames;
+		this.frameTimeInSecs = frameTimeInSecs;
+	}
+
+	public void update(float dtInSec) {
+		timerInSecs += dtInSec;
+		if (timerInSecs > frameTimeInSecs) {
+			timerInSecs = 0;
+			currentFrame = (currentFrame == nFrames-1) ? 0 : currentFrame + 1;
+		}
+	}
+
+	public void setCurrentFrame(int currentFrame) {
+		this.currentFrame = currentFrame;
+	}
+
+	public int getCurrentFrame() {
+		return currentFrame;
+	}
+}
--- core/src/com/gwenci/zarrax/game/BaseAlien.java
+++ core/src/com/gwenci/zarrax/game/BaseAlien.java
@@ -3,37 +3,36 @@ package com.gwenci.zarrax.game;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.gwenci.zarrax.Animator;
 import com.gwenci.zarrax.BaseActor;
 
 abstract class BaseAlien extends BaseActor {
-	AlienState state;
-	Texture alienTexture;
-	private int frameWidth;
-	private int nframes;
-	int halfFrameWidth;
-	int halfFrameHeight;
-	float animFrameRate;
-	float animFrameRateTimer = 0;
-	int animFrame = 0;
+
+	private AlienState state;
+	private final Texture alienTexture;
+	private final int frameWidth;
+	private final int halfFrameWidth;
+	private final int halfFrameHeight;
+
 	long lastTimeFired = TimeUtils.millis();
 
+	private final Animator animator;
 
-	BaseAlien(Texture texture, int nframes, float animFrameRate) {
+	BaseAlien(Texture texture, int nFrames, float animFrameRate) {
+
+		animator = new Animator(nFrames, animFrameRate);
 
 		alienTexture = texture;
-		frameWidth = texture.getWidth() / nframes;
-		this.nframes = nframes;
+		frameWidth = texture.getWidth() / nFrames;
 
 		halfFrameWidth = this.alienTexture.getWidth() / 2;
 		halfFrameHeight = this.alienTexture.getHeight() / 2;
 
-		animFrame = (int) (nframes * Math.random());
+		animator.setCurrentFrame((int) (nFrames * Math.random()));
 		setPosition(SCREEN_WIDTH / 2.0f - 16f, 300f);
 		setBoundingRect(frameWidth, texture.getHeight());
 		setState(AlienState.ALIVE);
-		this.animFrameRate = animFrameRate;
 	}
-
 
 	int getScore() {
 		return 0;
@@ -44,43 +43,37 @@ abstract class BaseAlien extends BaseActor {
 	}
 
 
-	 float getCentreX() {
+	float getCentreX() {
 		 return getX() + halfFrameWidth;
 	 }
 
 
-	 float getCentreY() {
+	float getCentreY() {
 		 return getY() + halfFrameHeight;
 	 }
 
 
-	 boolean isAlive() {
+	boolean isAlive() {
 		return state == AlienState.ALIVE;
 	 }
 
 
-	 public void act(float dt) {
+	public void act(float dt) {
 		super.act(dt);
-		animFrameRateTimer += dt;
-		 if (animFrameRateTimer > animFrameRate) {
-			 animFrameRateTimer = 0;
-			 animFrame--;
-			 if (animFrame < 0) animFrame = nframes-1;
-		 }
+		animator.update(dt);
 	}
 
 
 	public void draw(Batch batch, float parentAlpha) {
 		if (state != AlienState.ALIVE ) return;
 		super.draw(batch, parentAlpha);
-		batch.draw(alienTexture, getX(), getY(),
-				animFrame * frameWidth, 0,
-				frameWidth, this.alienTexture.getHeight() );
-	 }
+		batch.draw(alienTexture, getX(), getY(), animator.getCurrentFrame() * frameWidth, 0, frameWidth,
+				this.alienTexture.getHeight() );
+	}
 
 
-	 public abstract boolean canFire();
+	public abstract boolean canFire();
 
-	 public abstract boolean isFiring(float chanceToFire);
+	public abstract boolean isFiring(float chanceToFire);
 
  }
